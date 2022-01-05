01ad0367e899c7a28496d19d1d8563bebfa25c35
unknown <tamiloreolaniyi@gmail.com>
2022-01-05 11:38:40 +0100

    useEffect

--- src/EffectTutorial.jsx
+++ src/EffectTutorial.jsx
@@ -1,18 +1,27 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 function EffectTutorial() {
-
+    const[data, setData] = useState("");
+    const[count, setCount] = useState(0);
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/comments')
         .then((response) => {
-        console.log(response)})
-    })
+          setData(response.data[0].email);
+        console.log("API was called")
+        });
+    }, []);
 
     return (
         <div>
-           <h1>Hello World</h1> 
+           <h1>Hello World {data}</h1> 
+           <h1>{count}</h1>
+           <button
+           onClick={() => {
+               setCount(count + 1);
+           }}
+           >Click me</button>
         </div>
     )
 }
